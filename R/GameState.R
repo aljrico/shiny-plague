c2e554704ad5ba5d32337581364224b3208d7fa7
aljrico <aljrico@gmail.com>
2022-08-09 11:59:39 +0200

    fix: prevent extra spending

--- R/GameState.R
+++ R/GameState.R
@@ -202,7 +202,8 @@ GameState <- R6::R6Class(
     },
     buyCard = function(card) {
       increase_modifier <- 0.1
-      self$spendDNAPoints(card$getCost())
+      canAfford <- try(self$spendDNAPoints(card$getCost()))
+      if("try-error" %in% class(canAfford)) return(NULL)
       self$increaseLethality(by = card$getLethalityImpact() * increase_modifier)
       self$increaseVisibility(by = card$getVisibilityImpact() * increase_modifier)
       self$increaseInfectiousness(by = card$getInfectiousnessImpact() * increase_modifier)
