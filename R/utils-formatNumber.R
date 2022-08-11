ed24b337159ff08b053f13619399c71e520fae89
aljrico <aljrico@gmail.com>
2022-08-11 17:15:23 +0200

    chore: document format numbers function

--- R/utils-formatNumber.R
+++ R/utils-formatNumber.R
@@ -1,3 +1,8 @@
-formatNumbers <- function(x){
-  scales::label_number_si(accuracy = 1)(x)
+#' Format Numbers
+#'
+#'
+#' @param x number to be formatted
+#' @param accuracy accuracy parameter
+formatNumbers <- function(x, accuracy = 1){
+  scales::label_number_si(accuracy = accuracy)(x)
 }
