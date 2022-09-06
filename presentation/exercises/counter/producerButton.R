45d148e50cffbb66a5cf3d33fcbe5b3e8c95844b
aljrico <aljrico@gmail.com>
2022-09-06 15:03:03 +0100

    feat: producerButton

---
+++ presentation/exercises/counter/producerButton.R
@@ -0,0 +1,15 @@
+producerButton_ui <- function(id){
+  ns <- shiny::NS(id)  
+  actionButton(
+    inputId = ns('producer'),
+    label = 'Producer'
+  )
+}
+
+produerButton_server <- function(id){
+  shiny::moduleServer(id, function(input, output, session){
+    observeEvent(input$producer, {
+      print(input$producer)
+    })
+  })
+}
